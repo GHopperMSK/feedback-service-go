acfae02d660c5c94c3f66dd54bf0d6cd94adb5bc
Nikolai Poriadin <nikolai.poriadin@paxful.com>
2021-09-03 18:33:34 +0300

    fix kafka config

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"strconv"
@@ -20,55 +19,16 @@ import (
 
 const (
 	topic         = "test"
-	brokerAddress = "192.168.0.40:29092"
+	brokerAddress = "kafka:9092"
 )
 
 func main() {
-	addr, err := net.LookupIP("kafka:29092")
-	if err != nil {
-		fmt.Println("Unknown host")
-	} else {
-		fmt.Println("IP address: ", addr)
-	}
 	time.Sleep(5 * time.Second)
 	log.Println("Start server")
 
 	ctx := context.Background()
-	// go produce(ctx)
-	// go consume(ctx)
-
-	////////////////////////
-	i := 0
-
-	l := log.New(os.Stdout, "kafka writer: ", 0)
-	// intialize the writer with the broker addresses, and the topic
-	w := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{"192.168.0.40:29092"},
-		Topic:   "test",
-		// assign the logger to the writer
-		Logger: l,
-	})
-
-	for {
-		// each kafka message has a key and value. The key is used
-		// to decide which partition (and consequently, which broker)
-		// the message gets published on
-		err := w.WriteMessages(ctx, kafka.Message{
-			Key: []byte(strconv.Itoa(i)),
-			// create an arbitrary message payload for the value
-			Value: []byte("this is message" + strconv.Itoa(i)),
-		})
-		if err != nil {
-			panic("could not write message " + err.Error())
-		}
-
-		// log a confirmation once the message is written
-		fmt.Println("writes:", i)
-		i++
-		// sleep for a second
-		time.Sleep(time.Second)
-	}
-	////////////////////////
+	go produce(ctx)
+	go consume(ctx)
 
 	repository, err := mysql.New()
 	if err != nil {
@@ -131,7 +91,7 @@ func consume(ctx context.Context) {
 	r := kafka.NewReader(kafka.ReaderConfig{
 		Brokers: []string{brokerAddress},
 		Topic:   topic,
-		GroupID: "my-group",
+		// GroupID: "my-group",
 		// assign the logger to the reader
 		Logger: l,
 	})
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Deployment
+
+$ docker-compose up
+
+## Run tests
+
+$ docker-compose -f docker-compose.yml -f docker-compose.test.yml up
+
