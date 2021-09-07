3422e9b6f830b9f6a2f865f18cbabd2a596c42b8
Nikolai Poriadin <nikolai.poriadin@paxful.com>
2021-09-07 21:35:51 +0300

    kafka actions

--- handlers/kafka/kafka.go
+++ handlers/kafka/kafka.go
@@ -37,36 +37,96 @@ func Consume(ctx context.Context, repo repository.Repository) {
 			panic("could not read message " + err.Error())
 		}
 
-		var inputFeedback KafkaFeedback
-		err = json.Unmarshal(rawMsg.Value, &inputFeedback)
+		var inputRequest kafkaRequest
+		err = json.Unmarshal(rawMsg.Value, &inputRequest)
 		if err != nil {
 			panic(err.Error())
 		}
 
-		// TODO: check if inputFeedback.Version valid
+		fmt.Printf("type: %T, value: %v", inputRequest.Payload, inputRequest.Payload)
 
-		request := repository.FeedbackRequest{
-			ParentId:   inputFeedback.ParentId,
-			SenderId:   inputFeedback.SenderId,
-			ReceiverId: inputFeedback.ReceiverId,
-			TradeId:    inputFeedback.TradeId,
-			Message:    inputFeedback.Message,
-			Type:       inputFeedback.Type,
-			CreatedAt:  inputFeedback.CreatedAt,
+		switch inputRequest.Action {
+		case "create-action":
+			createFeedback(inputRequest.Payload, repo)
+		case "update-action":
+			updateFeedback(inputRequest.Payload, repo)
+		case "delete-action":
+			deleteFeedback(inputRequest.Payload, repo)
+		default:
+			panic("Unknown action")
 		}
 
-		if len(request.Validate()) == 0 {
-			repo.Create(&request)
-		} else {
-			log.Println("request validation error")
-		}
-
-		// after receiving the message, log its value
 		fmt.Println("sucessfully got:", string(rawMsg.Value))
 	}
 }
 
-type KafkaFeedback struct {
+func createFeedback(payload json.RawMessage, repo repository.Repository) {
+	// TODO: check if inputFeedback.Version valid
+	var inputData createRequest
+	err := json.Unmarshal(payload, &inputData)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	request := repository.FeedbackRequest{
+		ParentId:   inputData.ParentId,
+		SenderId:   inputData.SenderId,
+		ReceiverId: inputData.ReceiverId,
+		TradeId:    inputData.TradeId,
+		Message:    inputData.Message,
+		Type:       inputData.Type,
+		CreatedAt:  inputData.CreatedAt,
+	}
+
+	if len(request.Validate()) == 0 {
+		repo.Create(&request)
+	} else {
+		log.Println("request validation error")
+	}
+}
+
+func updateFeedback(payload json.RawMessage, repo repository.Repository) {
+	// TODO: check if inputFeedback.Version valid
+	var inputData updateRequest
+	err := json.Unmarshal(payload, &inputData)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	request := repository.FeedbackRequest{
+		ParentId:   inputData.ParentId,
+		SenderId:   inputData.SenderId,
+		ReceiverId: inputData.ReceiverId,
+		TradeId:    inputData.TradeId,
+		Message:    inputData.Message,
+		Type:       inputData.Type,
+		CreatedAt:  inputData.CreatedAt,
+	}
+
+	if len(request.Validate()) == 0 {
+		repo.Update(inputData.Id, &request)
+	} else {
+		log.Println("request validation error")
+	}
+}
+
+func deleteFeedback(payload json.RawMessage, repo repository.Repository) {
+	// TODO: check if inputFeedback.Version valid
+	var inputData deleteRequest
+	err := json.Unmarshal(payload, &inputData)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	repo.Delete(inputData.Id)
+}
+
+type kafkaRequest struct {
+	Action  string
+	Payload json.RawMessage
+}
+
+type createRequest struct {
 	Version    string
 	ParentId   int `json:"parent_id"`
 	SenderId   int `json:"sender_id"`
@@ -75,4 +135,21 @@ type KafkaFeedback struct {
 	Message    string
 	Type       string
 	CreatedAt  string `json:"created_at"`
+}
+
+type updateRequest struct {
+	Version    string
+	Id         int
+	ParentId   int `json:"parent_id"`
+	SenderId   int `json:"sender_id"`
+	ReceiverId int `json:"receiver_id"`
+	TradeId    int `json:"trade_id"`
+	Message    string
+	Type       string
+	CreatedAt  string `json:"created_at"`
+}
+
+type deleteRequest struct {
+	Version string
+	Id      int
 }
